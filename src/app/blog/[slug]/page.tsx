cdbcb5bb60243b7d95dbc364459767c2432a8385
Dirma-dayo <fahribaadila3@gmail.com>
2025-09-17 21:52:31 +0700

    added the youtueb link thing in slug blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { posts } from "../../../data/posts";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 
 export default function BlogPost({ params }: { params: { slug: string } }) {
   const post = posts.find((p) => p.slug === params.slug);
@@ -10,12 +11,13 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 
   return (
     <main className="min-h-screen bg-black text-green-400 font-mono p-6">
-  <article className="prose prose-invert prose-green max-w-none">
-    <h1 className="text-3xl font-extrabold">{post.title}</h1>
-    <p className="italic text-gray-400">{post.date}</p>
-    <ReactMarkdown>{post.content}</ReactMarkdown>
-  </article>
-</main>
-
+      <article className="prose prose-invert prose-green max-w-none">
+        <h1 className="text-3xl font-extrabold">{post.title}</h1>
+        <p className="italic text-gray-400">{post.date}</p>
+        <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+          {post.content}
+        </ReactMarkdown>
+      </article>
+    </main>
   );
 }
--- src/data/posts.ts
+++ src/data/posts.ts
@@ -21,4 +21,13 @@ i think i’m in an abusive relationship w/ pacman.
 i dont mind tho 6909MiB / 15687MiB ram usage is dope
     `,
   },
+  {
+    slug: "i-love-rythm-game",
+    title: "i really love arcaea",
+    date: "2025-09-05",
+    content: `
+istg- how do people do this- I CANT EVEN DO PRESENT ON TEMPEST LET ALONE A BEYOND ON THUMB PLAY
+<iframe width="560" height="315" src="https://www.youtube.com/embed/L2eUIEj3X54?si=cp0wYA7eTqY_Q8LY&amp;start=70" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    `,
+  },
 ];
