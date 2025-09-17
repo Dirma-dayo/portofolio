797be2213ef23cca063f619eae4ac925f222d2bb
Dirma-dayo <fahribaadila3@gmail.com>
2025-09-17 21:57:09 +0700

    added the youtueb link thing in slug blog

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -14,7 +14,7 @@ look at this beast.
   {
     slug: "arch-pain",
     title: "arch linux makes me suffer",
-    date: "2025-09-05",
+    date: "2025-09-12",
     content: `
 installed arch again today. broke it again. fixed it again.
 i think i’m in an abusive relationship w/ pacman.
@@ -22,12 +22,23 @@ i dont mind tho 6909MiB / 15687MiB ram usage is dope
     `,
   },
   {
+    slug: "syuh",
+    title: "i love doing syu",
+    date: "2025-09-15",
+    content: `
+did -Syu to upgrade my kernel to 5.16. grub broke and loaded to 5.15 so lmao nothing works not even nano
+ily arch for being so flexible i can litreally boot into it from a usb and load all my home files to fix it
+at least it works now
+    `,
+  },
+  {
     slug: "i-love-rythm-game",
     title: "i really love arcaea",
-    date: "2025-09-05",
+    date: "2025-09-17",
     content: `
 istg- how do people do this- I CANT EVEN DO PRESENT ON TEMPEST LET ALONE A BEYOND ON THUMB PLAY
 <iframe width="560" height="315" src="https://www.youtube.com/embed/L2eUIEj3X54?si=cp0wYA7eTqY_Q8LY&amp;start=70" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     `,
   },
+  
 ];
